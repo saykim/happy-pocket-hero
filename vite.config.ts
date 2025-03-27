afd42b289f5be93e808e785e534c4791673296eb
saykim <sangyeony+github@gmail.com>
2025-03-28 06:35:45 +0900

    vite.config.ts 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // <<< 이 줄 추가!
   server: {
     host: "::",
     port: 8080,
